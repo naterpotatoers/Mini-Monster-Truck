9fd0f0123cf364b235cea26ac00b11fa82040b8c
coreyboy1820 <coreyboy1820@hotmail.com>
2022-10-23 05:39:32 -0700

    working with serial

--- MMT-Arduino/src/main.cpp
+++ MMT-Arduino/src/main.cpp
@@ -5,6 +5,7 @@
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
 #include <DHT_U.h>
+#include <Servo.h>
 
 int sensor_values[5];
 // String commands = {"air":sensor_values[0],"soil":sensor_values[1],"heading":sensor_values[2],"speed":sensor_values[3],"angle":sensor_values[4]}
@@ -16,15 +17,17 @@ int sensor_values[5];
 #define Up 3
 #define Down 4
 #define Right 5
-#define DHTPIN A0
+#define DHTPIN 28
 #define pin_for_moisture_sensor A1
-#define DHTTYPE DHT22
+#define DHTTYPE DHT11
+#define servopin 13
 
 int pwm = 8;
 int direction = 26;
-int speed = 128;
+// int speed = 128;
 MPU9250 IMU(Wire, 0x68);
 DHT_Unified dht(DHTPIN, DHTTYPE);
+Servo myservo;
 
 struct magValues
 {
@@ -62,31 +65,38 @@ int GetMoisturePercent(int moisture_value)
   return 0;
 }
 
-dhtValues GetDhtValues(sensors_event_t event){
-  dhtValues dht;
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
-    dht.temp = event.temperature;
+dhtValues GetDhtValues(){
+  dhtValues dht_value;
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  // if (isnan(event.temperature)) {
+  //   Serial.println(F("Error reading temperature!"));
+  // }
+  // else {
+    dht_value.temp = event.temperature;
+    if(dht_value.temp > 0) {
     Serial.print(F("Temperature: "));
-    event.temperature;
-    Serial.print(event.temperature);
+    Serial.print(dht_value.temp);
     Serial.println(F("°C"));
-  }
+    }
+  // }
   // Get humidity event and print its value.
-  // dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
+  dht.humidity().getEvent(&event);
+  // if (isnan(event.relative_humidity)) {
+  //   Serial.println(F("Error reading humidity!"));
+  // }
+  // else {
+    dht_value.humidity = event.relative_humidity;
+    if(dht_value.humidity < 100) {
     Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
+    Serial.print(dht_value.humidity);
     Serial.println(F("%"));
-  }
+    }
+  // }
+    return dht_value;
 }
 
-void Move(int rdirection)
+void Move(int rdirection, int speed)
 {
   if (rdirection == 2)
   {
@@ -113,31 +123,37 @@ void setup()
   pinMode(pin_for_moisture_sensor, INPUT);
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
+  myservo.attach(servopin);
   // IMU.begin();
   // IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
   // IMU.setSrd(19);
-  Serial.begin(9600);
+  Serial.begin(38400);
 }
 void loop()
 {
-  // int heading = 0;
+  int heading = 0;
   // int moisture = 0;
+  int dir = 0;
+  int speed = 0;
+  int steer_angle = 0;
   // int moisture_percentage = 0;
   //  sensors_event_t event;
-  // dht.temperature().getEvent(&event);
-  // int dht_values = 0;
+  // dhtValues dht_values;
+  // magValues mag_values;
   // IMU.readSensor();
-  // if (Serial.available() > 0) {
-  //   String data = Serial.readStringUntil('\n');
-  //   Serial.print("You sent me: ");
-  //   Serial.println(data);
-  // }
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    Serial.print("You sent me: ");
+    Serial.println(data);
+  }
   // Serial.print("Hello There!!!");
   delay(1000);
-  Move(1); //pass in 0 for stopping, 1 for forward, and 2 for backwards
-  // heading = GetHeading(GetMagValues()); //heading has the heading from the magnometer
+  Move(dir, speed); //pass in 0 for stopping, 1 for forward, and 2 for backwards
+  myservo.write(steer_angle);
+  // heading = GetHeading(GetMagValues()); // heading has the heading from the magnometer
   // moisture = analogRead(pin_for_moisture_sensor); //will read the raw data for moisture
   // moisture_percentage = GetMoisturePercent(moisture); //will convert raw data of moisture to percent
-  // dht_values = analogRead(DHTPIN);
-  // Serial.print(dht_values);
+  // dht_values = GetDhtValues(); not working
+  // Serial.print(heading);
 }
