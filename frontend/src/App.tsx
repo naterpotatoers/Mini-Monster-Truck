5fde9636d501b643bb47772a26c313c4211439dd
naterpotatoers <nate.garza@hotmail.com>
2022-10-23 05:43:46 -0700

    removes unused var

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import { Button, Form } from 'react-bootstrap';
 import DataTable from './components/DataTable';
 // import MapContainer from './components/MapContainer';
